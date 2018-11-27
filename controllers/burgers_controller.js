4335e8aad80fcf6e66bc5a5190ca9d3e313df9d0
Jeme <aime.urquieta@outlook.com>
2018-11-27 08:12:10 -0600

    migrations to add drop down to hamburger table and routes to create or use new customer

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,74 +3,67 @@ const router = express.Router();
 const db = require('../models');
 
 //INDEX VIEW
-//get burgers' and customers' names to provide a drop down list of all burgers and customers' emails
+//get burgers' names to provide a drop down list of all burgers 
 router.get("/", (req, res) => {
-    db.Hamburgers.findAll({}).then((results) => {
+    db.Hamburgers.findAll({
+
+    }).then((results) => {
         res.render('index', { burgers: results });
     });
-
-    db.Customers.findAll({}).then((results) => {
-        res.render('index', { burgers: results });
-    });
 });
 
-//enter order by burger (name and quantity) and customer (email) 
-router.post('/api/burgers', (req, res) => {
-    db.Hamburgers.create({
-        burger_name: req.body.burger_name,
-    },
-        {
-            include:
-                [{
-                    association: Bill.Customer,
-                    include: Customer.Hamburgers
-                }]
-        }).then((results) => {
-            res.json(results);
-        });
+//ADD CUSTOMER IF IT'S NOT ALREADY ON THE LIST
+router.post('/api/customer', (req, res) => {
+    //finding if email exists
+    db.Customer.findOne({ where: {attr1: req.body.email} }).then(function(resultsEmail){
+    //if it exists redirect 
+        if (results) {
+        res.redirect('/api/customer/bill');
+    //if it doesn't create a new customer 
+        }else {
+            db.Customer.create({
+                email: req.body.email,
+            }).then((customerEmail) => {
+                res.redirect('/api/customer/bill');
+               // res.json(results);
+            })
+        }
+    })
+})
+   
 
-    db.Hamburgers.create({
-        burger_name: req.body.burger_name,
-    },
-        {
-            include:
-                [{
-                    association: Bill.Customer,
-                    include: Customer.Hamburgers
-                }]
-        }).then((results) => {
-            res.json(results);
-        })
-});
+    //enter a bill per hamburgers and customers
+    router.post('/api/customer/bill', (req, res) => {
+        db.Bill.create({
+            billTotal: req.body.billTotal,
+            hamburgers: [
+                { burger_name: req.body.burger_name },
+                { burger_name: req.body.burger_name },
+            ]
+        },
+            {
+                include:
+                    [Hamburgers]
+            }).then((results) => {
+                res.json(results);
+                //res.redirect('/');
+            })
 
-//ORDERS VIEW
-//show orders and check total per customer 
-router.get("/api/orders", (req, res) => {
-    db.Hamburgers.findAll({}).then((results) => {
-        res.render('orders', { burgers: results });
     });
 
-    db.Customer.findAll({}).then((results) => {
-        res.render('orders', { customer: results });
+    
+    //UPDATE BILLS IF CASHED
+    router.put("/api/customer/bill/:id", function (req, res) {
+        db.Bill.update({
+            cashed: req.body.cashed,
+        },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }).then(function () {
+                res.end();
+            })
     });
 
-    db.Bills.findAll({}).then((results) => {
-        res.render('orders', { check: results });
-    });
-});
-
-//update orders 
-router.put("/api/orders/:id", function (req, res) {
-    db.Hamburgers.update({
-        devoured: req.body.devoured,
-    },
-        {
-            where: {
-                id: req.params.id
-            }
-        }).then(function () {
-            res.end();
-        })
-});
-
-module.exports = router;
+    module.exports = router;
